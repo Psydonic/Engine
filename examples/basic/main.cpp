60922ad299ab251178cd8d351a054c7f12dfc5ee
ben <benbellavia123@gmail.com>
2025-01-12 23:21:22 +0000

    Added examples

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -1,5 +1,5 @@
 #include "Game.h"
-#include "Screen.h"
+#include "Screens/Screen.h"
 
 int main(void) {
 
--- include/Game.h
+++ include/Game.h
@@ -1,7 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "Screen.h"
+#include "Screens/Screen.h"
 
 class Game {
 public:
