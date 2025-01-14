d7f405a3300f8f056c8ecf0b32a2fb495c6a30cb
ben <benbellavia123@gmail.com>
2025-01-14 22:40:14 +0000

    updated widgets

--- include/UI/Widgets/Widget.h
+++ include/UI/Widgets/Widget.h
@@ -1,27 +1,26 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include "raylib.h"
+
 // A widget is a visual element that can be drawn on the screen.
 // It can be a simple shape, a text label, an image, or a more complex control.
 // It can be positioned, sized, and styled.
 
 class Widget {
 public: 
-  Widget(int x, int y, int width, int height);
   virtual void draw() const = 0;
-  void setPosition(int x, int y);
-  void setSize(int width, int height);
-  
-  int getX() const;
-  int getY() const;
-  int getWidth() const;
-  int getHeight() const;
+  virtual void update() = 0;
+
+  void setPosition(const Vector2& pos) { position = pos; }
+  void setSize(const Vector2& s) { size = s; }
+
+  const Vector2& getPosition() const { return position; }
+  const Vector2& getSize() const { return size; }
 
 protected:
-  int x;
-  int y;
-  int width;
-  int height;
+  Vector2 position;
+  Vector2 size;
 };
 
 #endif // WIDGET_H
